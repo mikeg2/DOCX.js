981cc4b95d1aa7e65506a0d86cd41d98928fba93
mikeg2 <michaelrgoldfine@gmail.com>
2014-02-14 22:06:48 -0500

    Create docx-to-html.js

---
+++ Demo/docx-to-html.js
@@ -0,0 +1,4 @@
+DOCX.js
+=======
+
+DOCX.js is a JavaScript library for converting the data in base64 DOCX files into HTML - and back! Please note that this library is licensed under the Microsoft Office Extensible File License - a license NOT approved by the OSI. While this license is based off of the MS-PL, which is OSI-approved, there are significant differences.
